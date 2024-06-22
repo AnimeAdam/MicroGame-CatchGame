944f26cc6f0af038d6b78dce16648d6d6a80f00b
AnimeAdam <Adam.Szlamp@gmail.com>
2024-06-22 13:58:14 +0100

    The ball will now follow a simple flight path.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -8,8 +8,8 @@ public class BallController : MonoBehaviour
     private const float scalingFactor = 10f; //How small the ball gets as it's moving
     private const float speed = 10f; //Speed the ball moves at
 
-    //End target point
-    public GameObject target;
+    
+    public GameObject target; //End target point
     
     //Flight path of the ball
     public GameObject flightPath;
@@ -21,11 +21,21 @@ public class BallController : MonoBehaviour
     void Start()
     {
         SelectTargetRandomLoc();
-        flightPathTargets = flightPath.GetComponentsInChildren<Transform>();
-        currentFlightPathTarget = 0;
         //TODO: Add a function that iterates through the flight paths starting at 1 ending at the length
+        InitialiseFlightPath();
+        BeginNextFlightPath();
+
         //Starts at 1 because 0 is the parent.
-        StartCoroutine(MoveObjectAtTime(5f, flightPathTargets[1].position));
+        // StartCoroutine(MoveObjectAtTime(5f, flightPathTargets[1].position));
+    }
+
+    private void BeginNextFlightPath()
+    {
+        currentFlightPathTarget++;
+        if (currentFlightPathTarget != flightPathTargets.Length)
+        {
+            StartCoroutine(MoveObjectAtTime(5f, flightPathTargets[currentFlightPathTarget].position));
+        }
     }
 
     // Update is called once per frame
@@ -48,22 +58,29 @@ public class BallController : MonoBehaviour
         target.transform.position = new Vector2(targetX, targetY);
     }
 
-    //Simplified tweening movement animation.
-    public IEnumerator MoveObjectAtTime(float timeOfArrival, Vector3 target)
+    //Setup the flight path to follow
+    private void InitialiseFlightPath()
     {
-        float duration = timeOfArrival - Time.time;
+        flightPathTargets = flightPath.GetComponentsInChildren<Transform>();
+        currentFlightPathTarget = 1;
+    }
+
+    //Simplified tweening movement animation.
+    public IEnumerator MoveObjectAtTime(float duration, Vector3 target)
+    {
+        // float duration = timeOfArrival; // - Time.time
         float distance = Vector3.Distance(transform.position, target);
         float speed = distance / duration;
  
-        while (transform.position.x != target.x && transform.position.y != target.y)
+        while (transform.position != target)
         {
             //Not sure about the Time.deltaTime, maybe try without and tweak it to see what results you get
             transform.position = Vector3.MoveTowards(transform.position, target, speed*Time.deltaTime);
             //wait for the frame to finish calculating
             yield return null;
         }
- 
         //stops the coroutine
+        Invoke("BeginNextFlightPath", 1.0f);
         yield break;
     }
 
