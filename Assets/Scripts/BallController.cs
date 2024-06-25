ebcd07f9447c97d9550733df256553d0c477ccba
AnimeAdam <Adam.Szlamp@gmail.com>
2024-06-25 18:28:57 +0100

    Ball will be destroyed from being too small or being catched.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    private const float rotateSpeed = 700f; //How fast it spins
-    private const float speed = 10f; //Speed the ball moves at
-    private const float shrinkingSpeed = 0.2f; //Speed the ball shrinks when thrown
-    
-    public GameObject target; //End target point
-    
-    //Flight path of the ball
+    //Ball Properties
+    private const float rotateSpeed = 900f; //How fast it spins.
+    private const float shrinkingSpeed = 0.2f; //Speed the ball shrinks when thrown.
+    private const float minimumSize = 0.06f; //Minimum size for the ball to be for the glove to catch it.
+        
+    //Flight path properties
     public GameObject flightPath;
     private Transform[] flightPathTargets;
     public float[] flightPathSpeeds;
     private int currentFlightPathTarget;
     private List<(float time, Transform transform)> flightPathCollection = new List<ValueTuple<float, Transform>>(); //Contains the flight path and time
+    public GameObject target; //End target point
+    private const float finalBallspeed = 2f; //Speed the ball moves at the final target.
 
     // Start is called before the first frame update
     void Start()
@@ -26,26 +27,12 @@ public class BallController : MonoBehaviour
         BeginNextFlightPath();
     }
 
-    private void BeginNextFlightPath()
-    {
-        currentFlightPathTarget++;
-        if (currentFlightPathTarget != flightPathCollection.Count)
-        {
-            StartCoroutine(MoveObjectAtTime(
-                flightPathCollection[currentFlightPathTarget].time
-                , flightPathCollection[currentFlightPathTarget].transform.position));
-        }
-        else
-        {
-            StartCoroutine(MoveObjectAtTimeOnce(2f, target.transform.position)); //FINAL target for the ball to reach.
-        }
-    }
-
     // Update is called once per frame
     void Update()
     {
         SpinningBall();
         ShrinkInSize();
+        DestoryBallFromShrinking();
     }
 
     //Spins the ball automatically.
@@ -60,6 +47,8 @@ public class BallController : MonoBehaviour
         if (transform.localScale.x > 0.001f)
             transform.localScale -= shrinkingSpeed * Time.deltaTime * Vector3.one;
     }
+    
+    #region FlightPath
 
     //Randomly sets the location of the target
     private void SelectTargetRandomLoc()
@@ -124,8 +113,36 @@ public class BallController : MonoBehaviour
         yield break;
     }
 
-    //Step 1: Animate swirl coming in, with scaling down.
-    //Step 2: Mid-point flys to target location.
+
+    //Call the next flight path target and the last one.
+    private void BeginNextFlightPath()
+    {
+        currentFlightPathTarget++;
+        if (currentFlightPathTarget != flightPathCollection.Count)
+        {
+            StartCoroutine(MoveObjectAtTime(
+                flightPathCollection[currentFlightPathTarget].time
+                , flightPathCollection[currentFlightPathTarget].transform.position));
+        }
+        else
+        {
+            StartCoroutine(MoveObjectAtTimeOnce(finalBallspeed, target.transform.position)); //FINAL target for the ball to reach.
+        }
+    }
+
+    #endregion FlightPath
+
+    private void DestoryBallFromShrinking()
+    {
+        if (transform.localScale.x < 0.01f)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player") && transform.localScale.x < minimumSize)
+            Destroy(gameObject);
+    }
+
     //Step 3: Destory after it's either too small or hits the glove.
     //Step 4: IF hit the glove then glove changes to catch animation.
 
